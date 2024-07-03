73829c76c6b6f97703cb91aa562f2ee313bd014f
sumukhthej <166727787+sumukhthej@users.noreply.github.com>
2024-07-03 21:16:55 +0900

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ const {
 module.exports = {
   development: {
     client: "postgresql",
-    connection: "postgresql://we_love_movies_cip7_user:T6cFNRN4RtMEbZvu9empkD7e54OKoiFp@dpg-cq2jr3dds78s73eg5dfg-a.oregon-postgres.render.com/we_love_movies_cip7?ssl=true",
+    connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -24,7 +24,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: "postgresql://we_love_movies_cip7_user:T6cFNRN4RtMEbZvu9empkD7e54OKoiFp@dpg-cq2jr3dds78s73eg5dfg-a.oregon-postgres.render.com/we_love_movies_cip7?ssl=true",
+    connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
